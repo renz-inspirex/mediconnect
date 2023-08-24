bd8ce3cbd730d95eff610d6a2d95c16ca48f8f20
Kenneth <renz@inspirex.io>
2023-08-24 10:17:50 +0800

    Fix hero component

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,12 +1,12 @@
 const HeroComponent = () => {
   return (
-    <div className="bg-secondary flex w-[100%] h-[815px]">
+    <div className="bg-secondary flex w-[100%] h-[815px] flex-col">
       <h1 className="text-white text-5xl w-[815px] h-[123px] text-center mx-auto  mt-[53px] font-bold font-sans proportional-nums lining-nums leading-[64px]">
         Welcome to your trusted source for reliable medical adviser
       </h1>
-      <div className="flex mt-[60px]">
-         <div className="container"></div>
-         <img src="../assets/images/logo.svg" />
+      <div className="flex mt-[60px] justify-center gap-52">
+         <div className="w-[710px] h-[384px] rounded-lg bg-white "></div>
+         <img src="../assets/images/doctor.svg" />
       </div>
     </div>
   )
