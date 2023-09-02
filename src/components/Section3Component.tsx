defea911ac434156c3b47d0d2a99548e7e3bb879
Jerald Sayson <saysonjerald@gmail.com>
2023-09-02 23:37:19 +0800

    section3 background has been updated

--- src/components/Section3Component.tsx
+++ src/components/Section3Component.tsx
@@ -7,9 +7,9 @@ const Section3Component = () => {
     return (
       <section className="flex w-[100%] h-[815px] flex-col" id="section3">
         <div className="flex mt-[60px] justify-around items-center gap-52 h-full">
-           <div className="w-[710px] h-[384px]">
-                <h2 className={`${ralewayHeading700.className} text-5xl text-dark m-4 leading-[63.99px]`}>Bridging the gap between Doctors and Patients</h2>
-                <p className={`${poppinesParagraph400.className} text-base text-ash m-6 leading-[24px]`}>Scheduling appointments with doctors has never been easier. Take control of your healthcare journey and never miss another important appointment.</p>
+           <div className="w-[710px] h-[384px] z-10">
+                <h2 className={`${ralewayHeading700.className} text-5xl text-dark m-4 leading-[63.99px]`}>Medical information and health advice you can trust</h2>
+                <p className={`${poppinesParagraph400.className} text-base text-ash m-6 leading-[24px]`}>Medconnect addresses the need for accessible and reliable medical information in a world where health decisions are becoming increasingly complex. By offering a user-friendly platform for symptom analysis, appointment scheduling, and medication availability</p>
            </div>
            <div></div>
         </div>
