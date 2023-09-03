e078c15178757358564a29279b69a6554f7240e2
Jerald Sayson <saysonjerald@gmail.com>
2023-09-04 02:41:42 +0800

    Added: Modal for Prompt Output on HeroComponent.tsx

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,20 +1,34 @@
 import { useState, useEffect, ChangeEvent } from "react"
 import { Roboto, Raleway } from "next/font/google"
-import SearchBar from "./_searchbarComponent"
+import HeroOutputModalComponent from "./HeroOutputModalComponent"
 
 const ralewayHeading700 = Raleway({ weight: '700', subsets: ['latin'], })
 const robotoParagraph400 = Roboto({ weight: '400', subsets: ['latin'], })
 
 const HeroComponent = () => {
 
+  // Textarea value
   const [value, setValue] = useState<string>('');
   const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value);
   };
 
+  // For Placeholder message
   const [placeHolderVisibility, setPlaceHolderVisibility] = useState(true);
 
+  // For Pleceholder blinking cursor
   const [count, setCount] = useState(0);
+
+  // Controling modal visibility
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  } 
 
   useEffect(() => {
     // Start the interval when the component mounts
@@ -28,6 +42,7 @@ const HeroComponent = () => {
 
   return (
     <div className="bg-secondary flex w-[100%] h-[815px] flex-col">
+      <HeroOutputModalComponent isOpen={isOpen} setIsOpen={setIsOpen} />
       <h1 className="text-white text-5xl w-[815px] h-[123px] text-center mx-auto  mt-[53px] font-bold font-sans proportional-nums lining-nums leading-[64px]">
         MedConnect - online medical hub for everyone
       </h1>
@@ -45,7 +60,7 @@ const HeroComponent = () => {
               <textarea className="w-full h-full outline-0 placeholder-black hero-textInput" value={value} onChange={handleTextareaChange}></textarea>
             )
           }
-            <button className="absolute bottom-6 right-6 py-4 px-24 bg-primary rounded-lg text-white ">Send</button>
+            <button className="absolute bottom-6 right-6 py-4 px-24 bg-primary rounded-lg text-white " onClick={openModal} disabled={!value}>Send</button>
          </div>
          <img src="../assets/images/doctor.svg" />
       </div>
