2ab8af2d2fb530e63b8ca8ffd0a1f267926c6dec
Kenneth <renz@inspirex.io>
2023-09-05 06:32:04 +0800

    Update page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html className="scroll-smooth" lang="en">
       <Head >
+      <title>Medconnect</title>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
